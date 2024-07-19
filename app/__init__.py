b02d40e89f91b9fa14da1c8a81787ce9249e8286
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-19 18:58:24 +0330

    Feat: changed the structure of the project & app configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .config import Config
-
+import os
 
 db = SQLAlchemy()
-migrate = Migrate() 
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object("config.Config")
 
     db.init_app(app)
     migrate.init_app(app, db)
-    from app.bot import models
+
+    with app.app_context():
+        from app.models.user import User
+        from app.models.order import Order
+        from app.models.subscription import Subscription
+
+        db.create_all()
 
     return app
