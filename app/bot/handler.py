bdce4c8bf974bdbc7cd774322c1b33c9af9913e4
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-22 23:09:23 +0330

    feat: registerd handlers

--- app/bot/handler.py
+++ app/bot/handler.py
@@ -7,3 +7,13 @@ def setup_handlers(bot: TeleBot):
     @bot.message_handler(commands=['start'])
     def handle_start(message):
         handler.start(message)
+    @bot.message_handler(func=lambda message: message.text == "💡 راهنما‌ی سرویس")
+    def tutorial(message):
+        handler.tutorial(message)
+    @bot.message_handler(func=lambda message: message.text == "💬 پشتیبانی")
+    def support(message):
+        handler.support(message)
+    @bot.message_handler(func= lambda message: message.text == "🧪دریافت سرور تست")
+    def test(message):
+        handler.test_sub(message)
+
