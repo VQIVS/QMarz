8ef61c0a7734eeef9df858ba841795baa236e49e
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-22 23:10:13 +0330

    feat: added inline keyboards

--- app/bot/keybaord.py
+++ app/bot/keybaord.py
@@ -1,5 +1,9 @@
 from telebot import types
+from ..repositories.tutorial_repository import TutorialRepository
 
+from app import create_app
+
+app = create_app()
 
 class KeyboardCreator:
     def __init__(self):
@@ -11,7 +15,20 @@ class KeyboardCreator:
         for button_row in buttons:
             keyboard.add(*button_row)
         return keyboard
-
+    
+    @staticmethod
+    def createInlineTutorial():
+        with app.app_context():
+            repo = TutorialRepository()
+            items = repo.get_all()
+            print(f"items: {items}")  
+            inline_keyboard_markup = types.InlineKeyboardMarkup()
+            for i, item in enumerate(items, start=1):
+                inline_button = types.InlineKeyboardButton(
+                    text=item.name, callback_data=f"{item.name}"
+                )
+                inline_keyboard_markup.add(inline_button)
+            return inline_keyboard_markup
 
 class Keyboard:
     def __init__(self):
@@ -29,4 +46,5 @@ class Keyboard:
             types.KeyboardButton("👨‍👩‍👧‍👧 معرفی به دوستان"),
         ],
     ]
-    mainKeyaboard = KeyboardCreator.create(mainButtons)
+    mainKeyboard = KeyboardCreator.create(mainButtons)
+    tutorialKeyboard = KeyboardCreator.createInlineTutorial()
