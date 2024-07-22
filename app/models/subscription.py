77f4f685859b241e9dfd697b6c611f6d093c635b
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-22 23:13:09 +0330

    feat: added subscription entity model

--- app/models/subscription.py
+++ app/models/subscription.py
@@ -5,9 +5,10 @@ class Subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plan = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    duration_months = db.Column(db.Integer, nullable=False)
+    duration_days = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    orders = db.relationship("Order", backref="subscription", lazy=True)
+    orders = db.relationship("Order", backref="subscription", lazy=True, cascade="all, delete-orphan")
+    
 
     def __repr__(self):
         return f"Subscription('{self.plan}', '{self.price}', '{self.duration_months} months')"
