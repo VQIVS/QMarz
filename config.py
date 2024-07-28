dc338dfab0c341da2bcb6fb339b8fdef2eaa832a
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-28 13:07:32 +0330

    added config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key")
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    DATABASE_PATH = os.path.join(BASE_DIR, "instance", "bot.db")
+    DATABASE_PATH = os.path.join(BASE_DIR,  "bot.db")
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
