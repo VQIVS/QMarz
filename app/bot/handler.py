13c6812747546c3d118f65fe388138a8fe7abc52
farnamah <145441216+farnamah@users.noreply.github.com>
2024-07-29 12:27:31 +0330

    feat(feat handler)adding a feature for handeling the joined

--- app/bot/handler.py
+++ app/bot/handler.py
@@ -1,19 +1,26 @@
 from telebot import TeleBot
 from app.bot.operation import ManiHandler
 
+
 def setup_handlers(bot: TeleBot):
     handler = ManiHandler(bot)
 
-    @bot.message_handler(commands=['start'])
+    @bot.message_handler(commands=["start"])
     def handle_start(message):
         handler.start(message)
+
     @bot.message_handler(func=lambda message: message.text == "💡 راهنما‌ی سرویس")
     def tutorial(message):
         handler.tutorial(message)
+
     @bot.message_handler(func=lambda message: message.text == "💬 پشتیبانی")
     def support(message):
         handler.support(message)
-    @bot.message_handler(func= lambda message: message.text == "🧪دریافت سرور تست")
+
+    @bot.message_handler(func=lambda message: message.text == "🧪دریافت سرور تست")
     def test(message):
         handler.test_sub(message)
 
+    @bot.callback_query_handler(func=lambda query: query.data == "joined")
+    def handle_join(query):
+        handler.handle_join(query)
