cdd07100d88104d437bc8b8e895e6fd4faca39b3
farnamah <145441216+farnamah@users.noreply.github.com>
2024-07-30 15:14:53 +0330

    feat(feat user model)add one field named points in the user model

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(120), unique=True, nullable=False)
+    points = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f"<User {self.user_id}>"
