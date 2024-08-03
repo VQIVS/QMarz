564ad73dee126fa2f9baccf1d800823da1d83f92
farnamah <145441216+farnamah@users.noreply.github.com>
2024-08-03 12:02:40 +0330

    feat(feat user repository)adding a def for getting all the user

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -3,16 +3,17 @@ from app import db
 
 
 class UserRepository:
-    def get_by_id(self, user_id):
-        return User.query.get(user_id)
-
-    def get_by_username(self, user_id):
-        return User.query.filter_by(user_id=user_id).first()
-
     def create(self, user_id):
-        if self.get_by_username(user_id):
-            raise ValueError("User with this username already exists")
-
+        user = User.query.filter_by(user_id=user_id).first()
+        if user:
+            return user
         new_user = User(user_id=user_id)
         db.session.add(new_user)
         db.session.commit()
+        return new_user
+
+    def get_by_user_id(self, user_id):
+        return User.query.filter_by(user_id=user_id).first()
+
+    def get_all_users(self):
+        return User.query.all()
