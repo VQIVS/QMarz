57f00fd13f8039c50da5170758e1b6a2ff1bd832
farnamah <145441216+farnamah@users.noreply.github.com>
2024-08-06 14:06:59 +0330

    feat(feat handler)adding feature for the confirming the photo

--- app/bot/handler.py
+++ app/bot/handler.py
@@ -1,11 +1,16 @@
 from telebot import TeleBot
+from telebot.types import CallbackQuery, Message
 from app.bot.operation import ManiHandler
 from app.bot.keybaord import Keyboard
-from telebot.types import CallbackQuery
 
 
 def setup_handlers(bot: TeleBot):
     handler = ManiHandler(bot)
+    keyboard = Keyboard()
+
+    @bot.message_handler(commands=["start"])
+    def handle_start(message):
+        handler.start(message)
 
     @bot.message_handler(func=lambda message: message.text == "⭐️ خرید سرویس")
     def buy_service(message):
@@ -15,9 +20,17 @@ def setup_handlers(bot: TeleBot):
     def service_selection(call: CallbackQuery):
         handler.handle_service_selection(call)
 
-    @bot.message_handler(commands=["start"])
-    def handle_start(message):
-        handler.start(message)
+    @bot.callback_query_handler(func=lambda call: call.data == "confirm")
+    def confirm(call: CallbackQuery):
+        handler.handle_confirm(call)
+
+    @bot.callback_query_handler(func=lambda call: call.data == "discount_code")
+    def discount_code(call: CallbackQuery):
+        handler.handle_discount_code(call)
+
+    @bot.callback_query_handler(func=lambda query: query.data == "joined")
+    def handle_join(query):
+        handler.handle_join(query)
 
     @bot.message_handler(func=lambda message: message.text == "💡 راهنما‌ی سرویس")
     def tutorial(message):
@@ -37,7 +50,7 @@ def setup_handlers(bot: TeleBot):
 
     @bot.message_handler(func=lambda message: message.text == "🏆 امتیازات")
     def points_menu(message):
-        handler.bot.send_message(message.chat.id, "Choose an option:", reply_markup=Keyboard.pointsKeyboard)
+        handler.bot.send_message(message.chat.id, "Choose an option:", reply_markup=keyboard.pointsKeyboard)
 
     @bot.message_handler(func=lambda message: message.text == "👀 مشاهده امتیازات")
     def show_points(message):
@@ -45,8 +58,12 @@ def setup_handlers(bot: TeleBot):
 
     @bot.message_handler(func=lambda message: message.text == "🔙 بازگشت به منو اصلی")
     def go_back_to_main(message):
-        handler.bot.send_message(message.chat.id, "Returning to the main menu.", reply_markup=Keyboard.mainKeyboard)
+        handler.bot.send_message(message.chat.id, "Returning to the main menu.", reply_markup=keyboard.mainKeyboard)
 
-    @bot.callback_query_handler(func=lambda query: query.data == "joined")
-    def handle_join(query):
-        handler.handle_join(query)
+    @bot.message_handler(content_types=['photo'])
+    def payment_picture(message: Message):
+        handler.handle_payment_picture(message)
+
+    @bot.message_handler(func=lambda message: True)
+    def payment_confirmation(message: Message):
+        handler.handle_payment_confirmation(message)
